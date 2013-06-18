4f2617cda88735589d01d451eaaeea9329f3492c
rheaton <rachelmheaton@gmail.com>
2013-06-17 21:35:20 -0400

    initial commit

---
+++ lib/match_media_js.rb
@@ -0,0 +1,8 @@
+require "match_media_js/version"
+
+module MatchMediaJs
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/match_media_js/version.rb
@@ -0,0 +1,3 @@
+module MatchMediaJs
+  VERSION = "0.0.1"
+end
